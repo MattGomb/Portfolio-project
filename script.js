b493ac0e43d6dcef33694d534d45ed8405bd6e2e
Matyas Gombos <gombosmm@gmail.com>
2022-09-07 08:41:08 -0500

    Add blur effect to mobile menu

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 function closeMenu() {
   document.getElementById('myButtons').style.display = 'none';
+  var blur = document.getElementById('blur');
+  blur.classList.toggle('active')
 }
 
 // eslint-disable-next-line no-unused-vars
 function openMenu() {
   document.getElementById('myButtons').style.display = 'block';
+  var blur = document.getElementById('blur');
+  blur.classList.toggle('active')
 }
